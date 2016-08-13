a4bada31c1c76bf0297a98041d113d29c8f5cb11
Todd Ruhlen <rhythmtome@yahoo.com>
2016-08-13 15:50:45 -0400

    Add token-based authorization for ActionCable connections

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+    
+    def connect
+      self.current_user = find_verified_user
+    end
+  
+  protected
+    def find_verified_user
+      if session = Session.find_by(token: request.params[:token])
+        session.user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
